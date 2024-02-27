80ee51a030e32ea3da0b80fc46a07a430f2011a0
SaiSahithPolimera <saisahithpoliera769@gmail.com>
2024-02-27 20:05:44 +0530

    Add routing and sign up pages

---
+++ src/pages/FacultySignup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FacultySignup = () => {
+  return (
+    <div>FacultySignup</div>
+  )
+}
+
+export default FacultySignup
---
+++ src/pages/components/Signup.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Signup = () => {
+
+  return (
+    <div className='back-div'>
+    <div className='signup-div'>
+        <button type="submit" className='sign-with-google'>Signup with Google</button>
+    </div>
+    </div>
+  );
+};
+
+export default Signup;
