c26e635553316430ddf010e09c064636a189fda4
SaiSahithPolimera <saisahithpoliera769@gmail.com>
2024-02-29 11:40:41 +0530

    Added proposal section for faculty

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,21 @@ import '../Styles/Home.css';
 import Header from './components/Header';
 import Hero from './components/Hero';
 
-
 const Home = () => {
   return (
     <>
-    <Header />
-    <Hero/>
+      <Header />
+      <Hero/>
+      <div className="login-buttons">
+        <button className="login-button">Faculty Login</button>
+        <button className="login-button">Student Login</button>
+      </div>
+      <div className="project-proposals">
+        <h2>Project Proposals</h2>
+        <p>Are you a student looking for a project? Or a faculty member seeking students for your project?</p>
+      </div>
     </>
   )
 }
-
 
 export default Home
